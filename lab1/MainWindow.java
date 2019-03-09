2e24aa87e2646aa6e4c8b8dcd13da2293fd74dbb
Tomasz Dziendzielski <tomasz.dziendzielski@gmail.com>
2019-03-09 14:39:49 +0100

    lab1: Jumping button

---
+++ lab1/MainWindow.java
@@ -0,0 +1,36 @@
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+public class MainWindow extends JFrame {
+
+    public static void main(String[] args) {
+
+        MainWindow frame = new MainWindow();
+        frame.setSize(400,300);
+        frame.setTitle("Jumping Button");
+
+        Random r = new Random();
+
+        JButton c = new JButton("Click me!");
+        c.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                c.setBounds(r.nextInt((int)frame.getSize().getWidth()-100), r.nextInt((int)frame.getHeight()-40), 90, 30);
+            }
+        });
+
+        JPanel content = new JPanel();
+        content.setLayout(null);
+
+        c.setBounds(70,70,90,30);
+        content.add(c);
+        frame.add(content);
+        frame.setVisible(true);
+    }
+}
+
+
