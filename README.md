e8448e62bba3df8808bebeb376fad42178d2e36c
Tomasz Dziendzielski <tomasz.dziendzielski@gmail.com>
2019-03-13 18:14:10 +0100

    lab2: Scheduler

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 lab1: aplikacja wyświetlająca przycisk, po naciśnięciu przycisk ma wędrować po oknie aplikacji (ale nie wychodzić)
+lab2: dodać scheduler, który zmieni tekst/rozmiar przycisku po określonym czasie (BackgroundWorkers, Timers, TimerTask)
--- lab1/MainWindow.java
+++ lab1/MainWindow.java
@@ -4,6 +4,8 @@ import javax.swing.JPanel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainWindow extends JFrame {
 
@@ -21,6 +23,7 @@ public class MainWindow extends JFrame {
             public void actionPerformed(ActionEvent arg0) {
                 c.setBounds(r.nextInt(frame.getWidth()-100),
                             r.nextInt(frame.getHeight()-40), 90, 30);
+                c.setText("Click me!");
             }
         });
 
@@ -34,5 +37,18 @@ public class MainWindow extends JFrame {
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
         frame.setUndecorated(true);
         frame.setVisible(true);
+
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                c.setText("DO IT!");
+                c.setBounds(c.getBounds().x+50,
+                            c.getBounds().y+50, 90, 30);
+            }
+        };
+
+        timer.schedule(task, 1000,2000);
+
     }
 }
