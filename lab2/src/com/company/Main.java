6ce370856a1aeff7e42df2861a25c4ca6227477e
Tomasz Dziendzielski <tomasz.dziendzielski@gmail.com>
2019-04-07 21:15:06 +0200

    lab2: Add MiG Layout

--- lab2/src/com/company/Main.java
+++ lab2/src/com/company/Main.java
@@ -1,20 +1,57 @@
 package com.company;
 
+import net.miginfocom.swing.MigLayout;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-
-public class Main {
+public class Main extends JFrame{
     public static void main(String[] args) {
+
+
+        Main frame = new Main();
+        frame.setSize(300,200);
+        frame.setTitle("Pogoda");
+        JButton btnPick = new JButton("Wybierz");
+        JTextArea txtCity = new JTextArea("<wpisz miasto>");
+        JTextArea labelData = new JTextArea("");
+        JLabel fillCity = new JLabel("Wpisz miasto:");
+
+
+        btnPick.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
         try {
-            String url = "http://api.openweathermap.org/data/2.5/weather?q=Warsaw&APPID=" + readFile("src/secret.txt", Charset.defaultCharset());
+            String getCity = txtCity.getText();
+            String url = "http://api.openweathermap.org/data/2.5/weather?q="+getCity+"&APPID=" + readFile("src/secret.txt", Charset.defaultCharset());
             Data data = ParseJSON.parse(GetJSON.downloadJSON(url));
             System.out.println(data);
+            labelData.setText(data.toString());
+
         } catch (IOException e) {
+            labelData.setText("Źle podana nazwa miasta");
             e.printStackTrace();
         }
+
+            }
+        });
+
+        JPanel content = new JPanel();
+        content.setLayout(new MigLayout("","[grow]","[grow][fill][][]"));
+
+        frame.getSize();
+        content.add(labelData,"cell 0 0, growx");
+        content.add(fillCity,"flowx, cell 0 1, align left, aligny center");
+        content.add(txtCity,"flowx, cell 0 1, alignx left, aligny center");
+        content.add(btnPick,"cell 0 2, growx");
+        frame.add(content);
+        frame.setVisible(true);
     }
 
     static String readFile(String path, Charset encoding) throws IOException {
--- lab2/src/com/company/ParseJSON.java
+++ lab2/src/com/company/ParseJSON.java
@@ -2,12 +2,18 @@ package com.company;
 
 import  org.json.*;
 
+import java.text.DecimalFormat;
+
 public class ParseJSON {
     public static Data parse(String str) {
         JSONObject obj = new JSONObject(str);
         String name = obj.getString("name");
         int id = obj.getInt("id");
-        float temp = obj.getJSONObject("main").getFloat("temp");
+
+        DecimalFormat df = new DecimalFormat("#.##");
+        String strTemp = df.format(obj.getJSONObject("main").getFloat("temp")-273.15).replace(',','.');
+
+        float temp = Float.valueOf(strTemp);
         float lon = obj.getJSONObject("coord").getFloat("lon");
         float lat = obj.getJSONObject("coord").getFloat("lat");
         return new Data(name, id, lon, lat, temp);
