0e86d9c0eb1be0d38f0151f434dffa3ef5dfcf74
Tomasz Dziendzielski <tomasz.dziendzielski@gmail.com>
2019-04-28 21:42:41 +0200

    Add database
    Adding data to database, create a chart base on database.
    Adding scheduler to update data and chart.

--- projekt/src/GUI.java
+++ projekt/src/GUI.java
@@ -1,31 +1,33 @@
-import javax.swing.*;
-
 import com.toedter.calendar.JCalendar;
-import org.jfree.data.xy.XYDataset;
-import org.jfree.chart.JFreeChart;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.time.*;
+import org.jfree.data.xy.XYDataset;
 
-import java.awt.BorderLayout;
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.*;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-
-import org.jfree.data.time.Day;
-import org.jfree.data.time.Hour;
-import org.jfree.data.time.TimeSeries;
-import org.jfree.data.time.TimeSeriesCollection;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class GUI {
     static JButton b, b1, b2;
     static JLabel l;
     Date selectedDate;
     String selectedCity;
-    String cities[] = {"Wroclaw", "Kraków", "Gdańsk"};
+    String cities[] = {"Wroclaw", "Warsaw", "London","Mexico"};
 
 
     public GUI() {
@@ -46,6 +48,9 @@ public class GUI {
             public void actionPerformed(ActionEvent actionEvent) {
                 selectedCity = cb.getItemAt(cb.getSelectedIndex()).toString();
                 city.setText("Wybrane miasto: "+ selectedCity);
+                XYDataset ds = createDataset();
+                ChartPanel cp = new ChartPanel(createChart(ds));
+                f.getContentPane().add(cp, BorderLayout.PAGE_END);
             }
         });
 
@@ -91,6 +96,18 @@ public class GUI {
         panel.add(label);
         f.add(panel);
 
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+             @Override
+             public void run() {
+                 XYDataset ds = createDataset();
+                 ChartPanel cp = new ChartPanel(createChart(ds));
+                 f.getContentPane().add(cp, BorderLayout.PAGE_END);
+             }
+         };
+
+         timer.schedule(task, 36000,36000);
+
     }
 
     private static JComboBox createCityList(String cities[]) {
@@ -98,17 +115,64 @@ public class GUI {
         return cb;
     }
 
-    private static XYDataset createDataset() {
+    static String readFile(String path, Charset encoding) throws IOException {
+        byte[] encoded = Files.readAllBytes(Paths.get(path));
+        return new String(encoded, encoding);
+    }
+
+
+    private XYDataset createDataset() {
+    try {
+        if (selectedCity != null) {
+            String url = "http://api.openweathermap.org/data/2.5/weather?q=" + selectedCity + "&APPID=" + readFile("/home/arch/java/secret.txt", Charset.defaultCharset());
+            com.company.ParseJSON.parse(com.company.GetJSON.downloadJSON(url));
+            //System.out.println(data);
+        }
+    }
+    catch (IOException e) {
+        e.printStackTrace();
+    }
+
         final TimeSeries series = new TimeSeries("Data", Hour.class);
         final Day today = new Day();
-        series.add(new Hour(1, today), 5.2);
-        series.add(new Hour(2, today), 6.1);
-        series.add(new Hour(3, today), 7.4);
-        series.add(new Hour(4, today), 3.2);
-        series.add(new Hour(7, today), 5.2);
-        series.add(new Hour(8, today), 10.2);
-        series.add(new Hour(12, today), 15.4);
-        series.add(new Hour(16, today), 11.2);
+        try(Connection conn = DriverManager.getConnection("jdbc:sqlite:temp.db"))
+        {
+            Statement statement = conn.createStatement();
+            String initDB = "CREATE TABLE IF NOT EXISTS temps(\n"
+                    + " id INTEGER PRIMARY KEY    AUTOINCREMENT,\n"
+                    + " city           TEXT    NOT NULL,\n"
+                    + " temp           FLOAT    NOT NULL,\n"
+                    + " temp_time      TEXT    NOT NULL);";
+            statement.execute(initDB);
+            ResultSet rs = statement.executeQuery("select * from temps where city='"+selectedCity+"'");
+            while(rs.next())
+            {
+                //System.out.println(rs.getInt("id") + " " + rs.getString("city") + " " +rs.getFloat("temp")+" " + rs.getString("temp_time"));
+                float temp = rs.getFloat("temp");
+                String string = rs.getString("temp_time");
+
+                String[] datetime = string.split(" ");
+                String[] dateparts = datetime[0].split("-");
+                int year = Integer.valueOf(dateparts[0]); // 004
+                int month = Integer.valueOf(dateparts[1]);
+                int day = Integer.valueOf(dateparts[2]);
+                String[] time = datetime[1].split(":");
+                int hour = Integer.valueOf(time[0]);
+                System.out.println(hour+" "+day+"/"+month+"/"+year+" "+ temp);
+                series.addOrUpdate(new Hour(hour,new Day(day,month,year)),temp);
+            }
+
+            // Dla jednej godziny może być przypisana tylko jedna wartość,
+            // więc na początku dodaję losową liczbę, by od razu było widać działający gotowy wykres,
+            // bez czekania aż uzupełni się baza danych.
+            series.addOrUpdate(new Hour(2,today),33);
+
+
+        }
+        catch(SQLException e) {
+            e.printStackTrace();
+        }
+
         final TimeSeriesCollection dataset = new TimeSeriesCollection(series);
         return dataset;
     }
